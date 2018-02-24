ccd5b2a6b18aa4bd112720f4c1d7c3ebbf596ad1
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-24 15:46:55 -0500

    bcrypt and validations added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password 
+  
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, :password, :username, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 8 }
 
   #find or create by oauth user_id
   def self.set_user_from_omniauth(uid)
