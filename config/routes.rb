764c82c16c77ab7fa6a542c8ba15ee4ebcc1e5f0
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-25 11:32:41 -0500

    director folder/files added

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :directors
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
