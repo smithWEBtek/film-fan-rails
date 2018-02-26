00f8cd2e167bc10b4fae88ca0f1c03b51b48ca6a
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-25 21:44:59 -0500

    updated director controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,2 +1,57 @@
 class DirectorsController < ApplicationController
+
+  before_action :set_director, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @directors = Director.all
+  end
+
+  def new
+    @director = Director.new
+  end
+
+  def create
+    @director = Director.new(director_params)
+    @director.user = current_user
+    if @director && @director.save
+      flash[:success] = "#{@director.name} added"
+      redirect_to directors_path(@director)
+    else
+      flash.now[:error] = "Please enter all fields"
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
+    if @director.update(director_params)
+      flash[:success] = "#{@director.name} was updated"
+      redirect_to director_path(@director)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @director.delete
+    flash[:success] = "#{@director.name} was deleted"
+    redirect_to directors_path
+  end
+
+  private
+
+    def set_director
+      @director = Director.find(params[:id])
+    end
+
+    def director_params
+      params.require(:director).permit(:name, :discovered, :rating, :notes)
+    end
+
 end
