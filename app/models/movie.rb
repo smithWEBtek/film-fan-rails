ff7974804175d54327b3937f17cd7e2f2fc01feb
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-26 13:14:10 -0500

    movie associations and validations added; added @director to path on movies/index

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  belongs_to :director
+  belongs_to :format
+
+  before_validation :make_title_case
+
+  validates_presence :title, :user_watched, :rating, :comments, :format
+
+  def make_title_case
+    self.title = self.title.titlecase
+  end
+  
 end
