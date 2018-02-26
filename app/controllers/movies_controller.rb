138a28a6462c8b7488eda6c52f78d642da2047b3
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-26 12:45:40 -0500

    movie controller actions added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,58 @@
 class MoviesController < ApplicationController
+   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+    
+  end
+
+  def create
+    @director = Director.find(params[:director_id]) #find the parent
+    @movie = @director.movies.build(movie_params)
+    if @movie && @movie.save
+      flash[:success] = "#{@movie.title} added"
+      redirect_to director_path(@director)
+    else
+      flash.now[:error] = "Please enter all fields"
+      render :new
+    end
+  end
+
+  def show
+    @director = Director.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:success] = "#{@movie.title} was updated"
+      redirect_to director_path(@director)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.delete
+    flash[:success] = "#{@movie.title} was deleted"
+    redirect_to movies_path
+  end
+
+  private
+
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:title, :user_watched, :rating, :comments, genre_id:[], :format_attributes => [:id, :name])
+    end
+
+
 end
