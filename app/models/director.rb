069962634d565d4783e57ea02729afb73d128620
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-25 21:49:02 -0500

    director model updated

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,13 @@
 class Director < ApplicationRecord
+
+  belongs_to :user
+
+  before_validation :make_title_case
+
+  validates_presence_of :name, :discovered, :rating
+
+  def make_title_case
+    self.name = self.name.titlecase
+  end
+
 end
