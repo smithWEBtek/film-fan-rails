6152b3fe18d9437efd9dc5dc8a9adeb265a71d48
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-25 22:01:05 -0500

    movies folders and files created

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :directors
+  resources :directors do
+    resources :artworks
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
