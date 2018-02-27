9db96f58e6941fbfdd54e38c71fd8c3dea03d2d4
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-27 11:10:54 -0500

    associations and validations added to Genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,12 @@
 class Genre < ApplicationRecord
+  has_many :movies
+  has_many :directors, through: :movies
+
+  before_validation :make_title_case
+  validates :name, presence: true
+
+  def make_title_case
+    self.name = self.name.titlecase
+  end
+  
 end
