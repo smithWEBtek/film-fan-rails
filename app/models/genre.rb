dff66cdd1905369a3ffac8de30cd34915ebd070e
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-27 13:10:30 -0500

    updates

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,7 @@ class Genre < ApplicationRecord
   has_many :directors, through: :movies
 
   before_validation :make_title_case
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def make_title_case
     self.name = self.name.titlecase
