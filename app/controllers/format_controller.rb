ec4bf7f6e3569276d698a64a556bb9057c81c80a
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-27 12:50:51 -0500

    format controller created and actions defined

---
+++ app/controllers/format_controller.rb
@@ -0,0 +1,57 @@
+class FormatController < ApplicationController
+
+  before_action :set_genre, only: [:show, :edit, :update, :destroy]
+  before_action :find_the_movie
+
+  def index
+    @format = Genre.all
+  end
+
+  def new
+    @Genre = Genre.new
+  end
+
+  def create
+    @genre = @movie.format.build(format_params)
+    if @genre && @genre.save
+      flash[:success] = "#{@genre.name} added"
+      redirect_to director_path(@director)
+    else
+      flash.now[:error] = "Please enter a genre"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @genre.update(format_params)
+      flash[:notice] = "#{@genre.name} was updated"
+    end
+  end
+
+  def destroy
+    @genre.delete
+  end
+
+  private
+
+    def find_the_movie
+      @movie = Movie.find(params[:movie_id]) #find the parent
+    end
+
+    def set_genre
+      @genre = Genre.find(params[:id])
+    end
+
+    def format_params
+      params.require(:genre).permit(:name)
+      # params.require(:format).permit(:name, movie_ids: [])
+    end
+
+
+end
