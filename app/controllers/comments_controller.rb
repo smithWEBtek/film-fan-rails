cbedd6fede66923724fa20ecb7a77cbf3dde408a
Eclecticdynastye <bklyngal1994@gmail.com>
2018-05-09 22:18:11 -0400

    comments controller created

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  before_action :set_movie
+
+  def index
+    @movie = Movie.find(params[:movie_id])
+    @comments = @movie.comments.all
+  end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:success] = 'Comment posted.'
+      redirect_to movie_path(@movie)
+    else
+      flash[:try_again] = "Something went wrong - please try again."
+      redirect_to movie_path(@movie)
+    end
+  end
+
+  def show
+    render 'com'
+  end
+
+
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+    
+  end
+  
+end
