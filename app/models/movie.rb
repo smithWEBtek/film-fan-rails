85e674163acdb25fff9b3d26821f495677345be3
Eclecticdynastye <bklyngal1994@gmail.com>
2018-05-10 10:08:03 -0400

    activerecord scope method re-written

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   belongs_to :genre
   has_many :comments
   
-
+  scope :newest_movies, -> { order('created_at desc').limit(5) }
   before_validation :make_title_case
 
   validates_presence_of :title, :year, :description, :director
@@ -23,21 +23,21 @@ class Movie < ApplicationRecord
     self.save
   end
 
-  def self.newest_movies
-    order('created_at desc').limit(5)    
-  end
+  # def self.newest_movies
+  #   order('created_at desc').limit(5)    
+  # end
 
   def make_title_case
     self.title = self.title.titlecase
   end
 
-  def genres_attributes=(genre_attributes)
-    genre_attributes.values.each do |genre_attr|
-      if !genre_attr[:name].blank?
-        genre = Genre.find_or_create_by(genre_attr)
-          self.genres << genre
-      end
-    end
-  end
+  # def genres_attributes=(genre_attributes)
+  #   genre_attributes.values.each do |genre_attr|
+  #     if !genre_attr[:name].blank?
+  #       genre = Genre.find_or_create_by(genre_attr)
+  #         self.genres << genre
+  #     end
+  #   end
+  # end
   
 end
