e10280e47bdf26f6687b19b5b7a40477601d9f6e
Eclecticdynastye <bklyngal1994@gmail.com>
2018-10-05 20:26:39 -0400

    add div new class to movies show page

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -38,23 +38,29 @@ function Comment(json) {
  Comment.prototype.renderP = function() {
    return "<p>" + this.body + "</p>"
 }
+
  $(function() {
   $("form#new_comment").on("submit", function(e) {
     e.preventDefault();
     var $form = $(this);
     var action = $form.attr("action");
     var params = $form.serialize();
+
      $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
+        url: action,
+        data: params,
+        dataType: "json",
+        method: "POST"
       })
       .done(function(json) {
+
        var comment = new Comment(json)
+
        var commentP = comment.renderP()
+
        $(".new").append(commentP)
      })
+      
     .error(function(response) {
       console.log("error!", response)
     })
